912b3f2f6395a17bdffa7bfe21f19ccb32ccc1e1
Ranar Basnet <basnetranar@gmail.com>
2018-07-19 13:28:57 +1000

    sdfs

--- src/home/home.js
+++ src/home/home.js
@@ -12,6 +12,8 @@ angular.module('myApp.home', ['ngRoute'])
 
 .controller('homeController', ['$scope','$http','$interval','$timeout',function($scope,$http,$interval,$timeout) {
 	
+    $scope.byClient = ""; //To change the heading based on selected option
+
 //Hide the client's name select option when client is not selected
 	$scope.hideOptionalSelect = function(){
         $scope.byClient = "";
@@ -20,18 +22,23 @@ angular.module('myApp.home', ['ngRoute'])
         $scope.byClient = "";}
         
 		else{
-			$scope.byClient = "by Client";
+			$scope.byClient = "by Client ";
 			document.getElementById('optionSelect').style.visibility = 'visible';
 		}
 	}
+
+    //Sample Displayed Graph
 	
 	  $scope.fruits = {
 	  	// labels data representated
-                labels: ["Mango", "Orange", "Peach"],
+                labels: ["Submitted","Not Yet Submitted"],
+                labels1: [ "Submitted","Still on time", "OverDue"],
          // data values
-                data: [20, 40, 55],
+                data: [60, 40],
+                data1: [60, 20, 20],
               // representational color
-                color: ["#FEBD01", "#FF8C00", "#FFCBA6"]
+                color: [ "#41bd3d" ,"#c1c1c3"],
+                color1: ["#41bd3d", "#145bf5", "#f71302"]
             };
             var canvas1 = document.getElementById("canvas1").getContext('2d');
             var canvas2 = document.getElementById("canvas2").getContext('2d');
@@ -41,9 +48,19 @@ angular.module('myApp.home', ['ngRoute'])
                 data: {
                     datasets: [{
                         data: $scope.fruits.data,
-                        backgroundColor: $scope.fruits.color
-                    }],
+                        backgroundColor: $scope.fruits.color,
+                        borderWidth: 0,
+                    
                     labels: $scope.fruits.labels
+                    },
+
+                    {
+                        data: $scope.fruits.data1,
+                        backgroundColor: $scope.fruits.color1,
+                        borderWidth: 0,
+                    labels: $scope.fruits.labels1
+                    }]
+
                 },
                 options: {
                     responsive: false
@@ -59,7 +76,16 @@ angular.module('myApp.home', ['ngRoute'])
                     labels: $scope.fruits.labels
                 },
                 options: {
-                    responsive: false
+                    responsive: true
                 }
             });
+
+
+
+        // Features included in the chart 1
+        // Not Yet Submitted , Submitted, 
+        //Second diagram included  Still on Time, Overdue, Submitted
+
+        //Featuers included in chart 2
+        // Ready to Submit , All submitted 
 }]);
